06172b05ad3b4c1a670eade17cca1f80f32fa3a5
Vanderlei Lopes <vanderleixjunior@gmail.com>
2018-06-04 15:45:17 -0300

    Initial Commit

---
+++ HelloUniverse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloUniverse
+//
+//  Created by Vanderlei Lopes De Oliveira Junior on 04/06/18.
+//  Copyright © 2018 Vanderlei Lopes De Oliveira Junior. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
